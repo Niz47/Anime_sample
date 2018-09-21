5ec8e2a8c5564d857427533bf6154bb366a59463
Zin Mar Htun <zinmarhtun@reddotnetwork.com>
2018-09-21 11:02:12 +0630

    add role for admin + default user and create Admin role user in db seed

--- WatchAndShare/Migrations/Configuration.cs
+++ WatchAndShare/Migrations/Configuration.cs
@@ -1,9 +1,13 @@
 namespace WatchAndShare.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
+    using System.Data.Entity.Validation;
     using System.Linq;
+    using WatchAndShare.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<WatchAndShare.Models.ApplicationDbContext>
     {
@@ -12,8 +16,10 @@ namespace WatchAndShare.Migrations
             AutomaticMigrationsEnabled = true;
         }
 
-        protected override void Seed(WatchAndShare.Models.ApplicationDbContext context)
+        protected override void Seed(ApplicationDbContext context)
         {
+            CreateRolesandUsers(context);
+
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
@@ -26,6 +32,61 @@ namespace WatchAndShare.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+        }
+
+        // In this method we will create default User roles and Admin user for login   
+        private void CreateRolesandUsers(ApplicationDbContext context)
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            try
+            {
+                // In Startup iam creating first Admin Role and creating a default Admin User    
+                if (!roleManager.RoleExists("Admin"))
+                {
+                    // first we create Admin rool   
+                    var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                    role.Name = "Admin";
+                    roleManager.Create(role);
+
+                    //Here we create a Admin super user who will maintain the website                  
+
+                    var user = new ApplicationUser();
+                    user.UserName = "admin@gmail.com";
+                    user.Email = "admin@gmail.com";
+
+                    string userPWD = "$Hwe123";
+
+                    var chkUser = UserManager.Create(user, userPWD);
+
+                    //Add default User to Role Admin   
+                    if (chkUser.Succeeded)
+                    {
+                        var result1 = UserManager.AddToRole(user.Id, "Admin");
+
+                    }
+                }
+
+                // creating Creating Employee role    
+                if (!roleManager.RoleExists("Default User"))
+                {
+                    var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                    role.Name = "Default User";
+                    roleManager.Create(role);
+
+                }
+            }
+            catch (DbEntityValidationException dbEx)
+            {
+                foreach (var validationErrors in dbEx.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        System.Console.WriteLine("Property: {0} Error: {1}", validationError.PropertyName, validationError.ErrorMessage);
+                    }
+                }
+            }
         }
     }
 }
